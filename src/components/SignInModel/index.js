3f35b25c47936b1872a1c0b827677fa4a0661def
ouhammmourachid <rachidouhammou21@gmail.com>
2023-09-18 14:16:25 +0100

    fixing signup and login connection

--- src/components/SignInModel/index.js
+++ src/components/SignInModel/index.js
@@ -1,13 +1,51 @@
 "use client"
-import React, { useEffect,useRef} from 'react';
+import React, { useEffect,useRef, useState} from 'react';
+import BASE_URL from '@/constants';
 
 export default function SignInModal({visible,setShowModel}){
     let signIn = useRef();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+      });
+    const [error, setError] = useState('');
+
+    // Handle form input changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+        ...formData,
+        [name]: value,
+        });
+    };
+
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+        try {
+            const response = await fetch(`${BASE_URL}/api/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            });
+            if (response.ok) {
+              const data = await response.json();
+              console.log(data);
+            } else {
+                setError('Please check your username and password and try again.');
+            }
+        } catch (error) {
+            console.error('An error occurred', error);
+        }
+    };
     useEffect(()=>{
         let handler =(e)=>{
             if(signIn.current && !signIn.current.contains(e.target)){
                 setShowModel(false);
-                console.log(signIn.current);
+                //console.log(signIn.current);
             }
         };
         document.addEventListener("mousedown",handler);
@@ -18,27 +56,57 @@ export default function SignInModal({visible,setShowModel}){
     if(!visible) return null;
     return(
     <div className='fixed inset-0 backdrop-blur-sm flex justify-center items-center'>     
-        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-xl shadow sm:p-6 md:p-8 dark:bg-colordark dark:border-gray-700">
+        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-xl shadow">
             <form class="space-y-6" ref={signIn}>
-                <h5 class="text-xl font-medium text-gray-900 dark:text-black">Sign in to shiny</h5>
+                <h5 class="text-xl font-medium text-gray-900 dark:text-black">Sign in to Pixel alchemy</h5>
+                {error && 
+                    <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-xs'>
+                        <strong class="font-bold">Login Failed:</strong>
+                        <span class="block sm:inline">{error}</span>
+                    {error}
+                    </div>
+                }
                 <div>
                     <label  class="block mb-2 text-sm font-medium text-secondary">Your email</label>
-                    <input type="email" name="email"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 " placeholder="name@company.com" required/>
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id='email'
+                    value={formData.email}
+                    onChange={handleInputChange}
+                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 " 
+                    placeholder="name@company.com" 
+                    required/>
                 </div>
                 <div>
                     <label  class="block mb-2 text-sm font-medium text-secondary">Your password</label>
-                    <input type="password" name="password"  placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" required/>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password"
+                    value={formData.password}
+                    placeholder="••••••••"
+                    onChange={handleInputChange}
+                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" 
+                    required/>
                 </div>
                 <div class="flex items-start">
                     <div class="flex items-start">
                         <div class="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                            <input 
+                            id="remember" 
+                            type="checkbox" 
+                            value="" 
+                            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" 
+                            required />
                         </div>
                         <label for="remember" class="ml-2 text-sm font-medium text-secondary">Remember me</label>
                     </div>
                     <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                 </div>
-                <button type="submit" class="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                <button 
+                type="submit" class="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={handleSubmit}>Login to your account</button>
             </form>
         </div>
     </div>
--- src/components/SignUpModel/index.js
+++ src/components/SignUpModel/index.js
@@ -1,14 +1,61 @@
 "use client"
-import React, { useEffect,useRef} from 'react';
-
+import React, { useEffect,useRef, useState} from 'react';
+import BASE_URL from '@/constants';
 
 export default function SignUpModal({visible,setShowModel}){
     let signUp = useRef();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        username:'',
+        name:''
+    });
+    const [error, setError] = useState('');
+    // Handle form input changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        // If the changed input is 'email', update 'username' as well
+        if (name === 'email') {
+            setFormData({
+            ...formData,
+            [name]: value,
+            username: value, // Set 'username' to the same value as 'email'
+            });
+        } else {
+            setFormData({
+            ...formData,
+            [name]: value,
+            });
+        }
+    };
+
+    // Handle form submission
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setError('');
+        try {
+            const response = await fetch(`${BASE_URL}/api/register`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            });
+            if (response.ok) {
+              const data = await response.json();
+            } else {
+                setError('Registration failed try an other email.');
+            }
+        } catch (error) {
+            console.error('An error occurred', error);
+        }
+    };
+
     useEffect(()=>{
         let handler =(e)=>{
             if(signUp.current &&!signUp.current.contains(e.target)){
                 setShowModel(false);
-                console.log(signUp.current);
+                setError('');
             }
         };
         document.addEventListener("mousedown",handler);
@@ -21,16 +68,50 @@ export default function SignUpModal({visible,setShowModel}){
     <div className='fixed inset-0 backdrop-blur-sm flex justify-center items-center'>     
         <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-xl shadow ">
             <form className="space-y-6" ref={signUp}>
-                <h5 className="text-xl font-medium text-gray-900 dark:text-black">Sign up to shiny</h5>
+                <h5 className="text-xl font-medium text-gray-900 dark:text-black">Sign up to Pixel alchemy</h5>
+                {error && 
+                    <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'>
+                        <strong className="font-bold">SignUp Failed:</strong>
+                        <span className="block sm:inline">{error}</span>
+                    {error}
+                    </div>
+                }
+                <div>
+                    <label  className="block mb-2 text-sm font-medium text-secondary">Your name</label>
+                    <input 
+                    type="name" 
+                    name="name"  
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 " 
+                    placeholder="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required/>
+                </div>
                 <div>
                     <label  className="block mb-2 text-sm font-medium text-secondary">Your email</label>
-                    <input type="email" name="email"  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 " placeholder="name@company.com" required/>
+                    <input 
+                    type="email" 
+                    name="email"  
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 " 
+                    placeholder="name@company.com"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required/>
                 </div>
                 <div>
                     <label  className="block mb-2 text-sm font-medium text-secondary">Your password</label>
-                    <input type="password" name="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" required/>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400" 
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required/>
                 </div>
-                <button type="submit" className="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create an account</button>
+                <button 
+                type="submit" 
+                className="w-full text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={handleSubmit}>Create an account</button>
             </form>
         </div>
     </div>
