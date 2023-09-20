61e2f6eb6ab40679ab951ec8b68957fb424d74be
ouhammmourachid <rachidouhammou21@gmail.com>
2023-09-20 17:00:34 +0100

    add log for logout

--- src/components/DropDown/index.js
+++ src/components/DropDown/index.js
@@ -39,7 +39,12 @@ export default function DropDown({showMenu,setShowMenu}){
                     </li>
                 </ul>
                 <div className="py-2">
-                    <Link href='#'  className="block px-4 py-2 text-sm text-red-500" onClick={handleLogOut}>Sign out</Link>
+                    <Link href='#' className="inline-flex items-center text-white bg-red-500 hover:bg-red-600 px-2 py-1 ml-3 rounded-md transition-colors duration-300 ease-in-out" onClick={handleLogOut}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
+                        </svg>
+                        Logout
+                    </Link>
                 </div>
             </div>
         </div>
