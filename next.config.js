cf0fad8e0a86e0d2a57b69b43b169691bdb5ceb6
ouhammmourachid <rachidouhammou21@gmail.com>
2023-10-11 17:00:24 +0100

    dessable eslintrc and activate exportation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ['127.0.0.1','102.37.17.20'], // Add the hostname here
       },
