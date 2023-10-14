2cff9c954166e2e555656fa44f728cd72d2c1554
ouhammmourachid <93659459+ouhammmourachid@users.noreply.github.com>
2023-10-14 14:41:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true,
     output: 'export',
     images: {
         domains: ['127.0.0.1','102.37.17.20'], // Add the hostname here
