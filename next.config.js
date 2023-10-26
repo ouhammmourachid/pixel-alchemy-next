f066576431076bd50c6ba58ab1a4e0ca60d22afd
ouhammmourachid <93659459+ouhammmourachid@users.noreply.github.com>
2023-10-26 11:48:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: 'http://ouhammmourachid.github.io/pixel-alchemy-next/',
-    basePath: '/pixel-alchemy-next',
     output: 'export',
     images: {
         domains: ['127.0.0.1','102.37.17.20'], // Add the hostname here
